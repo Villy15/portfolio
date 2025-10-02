27e50f8b6822c8782e02d3f0521bcc09adf27958
Villy15 <adrianvill07@gmail.com>
2025-10-02 16:05:51 +0800

    chore: add shadcn in ui package and use eslint in package

--- apps/portfolio/app/layout.tsx
+++ apps/portfolio/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+import "@repo/ui/globals.css";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
